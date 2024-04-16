50227069e1c61bf9bfd023c9bc1e2fa7002e41eb
Shoban Chiddarth <messageshoban@gmail.com>
2024-04-16 16:00:37 +0530

    Set gemini to block nothing

--- gemini-pro-vision-test.py
+++ gemini-pro-vision-test.py
@@ -16,20 +16,20 @@ generation_config = {
 safety_settings = [
   {
     "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_ONLY_HIGH"
+    "threshold": "BLOCK_NONE"
   },
   {
     "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_ONLY_HIGH"
+    "threshold": "BLOCK_NONE"
   },
   {
     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_ONLY_HIGH"
+    "threshold": "BLOCK_NONE"
   },
   {
     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  }
+    "threshold": "BLOCK_NONE"
+  },
 ]
 
 model = genai.GenerativeModel(model_name="gemini-pro-vision",
